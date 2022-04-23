85c3cf1cd4cd597b523ac09dea5c04236ffd1951
Mario Schmoker <103613451+MarioSchmoker@users.noreply.github.com>
2022-04-22 23:39:23 -0300

    Initial commit

---
+++ src/laGranjera3.ts
@@ -0,0 +1,9 @@
+console.log("girar a la izquierda 90°");
+console.log("avanzar");
+console.log("girar a la derecha 90°");
+console.log("avanzar");
+console.log("avanzar");
+console.log("girar a la derecha 90°");
+console.log("avanzar");
+console.log("quitar1");
+console.log("logrado");
